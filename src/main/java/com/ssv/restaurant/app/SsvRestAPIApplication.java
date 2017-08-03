a7acbfc3479f88fd7254a0106aff5aefa8627df2
Sivasankar-c <sivasankar.cs@gmail.com>
2017-08-03 01:31:56 -0400

    Initial Setup Commit

---
+++ src/main/java/com/ssv/restaurant/app/SsvRestAPIApplication.java
@@ -0,0 +1,12 @@
+package com.ssv.restaurant.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsvRestAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsvRestAPIApplication.class, args);
+	}
+}
